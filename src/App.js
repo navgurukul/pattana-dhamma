3f5ae806f505a6722cd8c4141488bc4c29c8885b
komalahire <komala@navgurukul.org>
2024-08-08 17:42:35 +0530

    created home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Header from './header';
+import Header from './Pages/Header/header';
+
+import Home from './Pages/home';
 
 function App() {
   return (
-   <Header />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* Add more routes here as needed */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css'; // Import your stylesheet
+
+const Home = () => {
+    return (
+        <div className="container" >
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-10 col-lg-12 col-xl-12 text-center">
+                    <h3 className="custom-heading">Dhamma Pattana Vipassana Centre</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
