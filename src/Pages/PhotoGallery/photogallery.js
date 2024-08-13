94c375aac07ed56f3d9783c25258e8245bca2ef0
ujala <ujalasaini355@gmail.com>
2024-08-13 15:50:55 +0530

    change the text size and remove the space, give the same photos extensions

--- src/Pages/PhotoGallery/photogallery.js
+++ src/Pages/PhotoGallery/photogallery.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import img1 from "./assets/img1.jpg";
-import img2 from "./assets/img2.jpeg";
-import img3 from "./assets/img3.jpeg";
-import img4 from "./assets/img4.jpeg";
-import img5 from "./assets/img5.jpeg";
-import img6 from "./assets/img6.jpeg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpg";
+import img4 from "./assets/img4.jpg";
+import img5 from "./assets/img5.jpg";
+import img6 from "./assets/img6.jpg";
 import img7 from "./assets/img7.jpg";
-import img8 from "./assets/img8.jpeg";
-import img10 from "./assets/img10.jpeg";
-import img11 from "./assets/img11.jpeg";
+import img8 from "./assets/img8.jpg";
+import img10 from "./assets/img10.jpg";
+import img11 from "./assets/img11.jpg";
 import img12 from "./assets/img12.jpg";
 import "./photogallery.css";
 
@@ -21,8 +21,8 @@ const ImageGallery = () => {
         { src: img3, alt: 'Image 3', text: 'A/c Meditation Hall' },
         { src: img4, alt: 'Image 4', text: 'Individual Meditation cushions' },
         { src: img5, alt: 'Image 5', text: 'Individual rooms' },
-        { src: img7, alt: 'Image 2', text: 'All residential rooms are equipped with A/C' },
-        { src: img6, alt: 'Image 1', text: 'Modern Hygenic Kitchen' },
+        { src: img6, alt: 'Image 2', text: 'All residential rooms are equipped with A/C' },
+        { src: img7, alt: 'Image 1', text: 'Modern Hygenic Kitchen' },
         { src: img8, alt: 'Image 3', text: 'Solar Heater for hot water supply' },
         { src: img10, alt: 'Image 5', text: 'Dining hall' },
         { src: img11, alt: 'Image 4', text: 'Dining hall' },
@@ -38,14 +38,13 @@ const ImageGallery = () => {
     };
 
     return (
-        <div className="container py-4">
+        <div className="container" style={{ maxWidth: "1200px" }}>
             <div className="row justify-content-center">
                 <h3 className="custom-heading text-center mt-4 mb-5">Photo Gallery</h3>
                 {images.map((image, index) => (
                     <div
-                        className={`${
-                            index === images.length - 1 ? 'col-12' : 'col-md-5 mx-4'
-                        } mb-4`}
+                        className={`${index === images.length - 1 ? 'col-12' : 'col-md-5 mx-2'
+                            } mb-1`}
                         key={index}
                     >
                         <div className="card h-100 border-0">
@@ -57,7 +56,7 @@ const ImageGallery = () => {
                                 style={{ objectFit: 'cover', width: '100%', height: '350px' }}
                             />
                             <div className="card-body p-0">
-                                <h3 className="h3 mb-3 text-center mt-2">{image.text}</h3>
+                                <p className="mb-3 text-center mt-2 imgText">{image.text}</p>
                             </div>
                         </div>
                     </div>
